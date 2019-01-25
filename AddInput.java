ae758e08204694aadde949dcbd2440c0e249d533
Justin <37247743+globalkitteh@users.noreply.github.com>
2019-01-25 09:02:41 -0600

    Completed Adder
    I did the thing (again)

--- AddInput.java
+++ AddInput.java
@@ -11,15 +11,44 @@ public class AddInput {
         // if (??? != 2) {
         //    do something
         // }
-        // More advanced: What if the two inputs aren't integers? Then what? How cold you check that?
+        // More advanced: What if the two inputs aren't integers? Then what? How could you check that?
     
         // **** START HERE: ****
-        // These two lines will get you the first two arguments 
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int sum = 
+        // These two lines will get you the first two arguments
+       //---------------------------------------------------------------------
+      //Defining Variables
+      int a; //input
+      int b; //input
+      int errorState = 0; //changed if errored
+      int numArgs = args.length; //number of inputs
         
-        // Print out "The sum of _ and _ is _" where you actually print the numbers they enter and the sum
-
+      try{  
+       Integer.parseInt(args[0]); //Testing the inputs to make sure they are 2 numbers not exceeding the limits of 'int'
+       Integer.parseInt(args[1]);        
+      }
+      catch(RuntimeException ex){ //Catching any input exceptions
+        if(numArgs == 1){
+          System.out.println("This program requires 2 values to function.");
+        }
+        else if(numArgs == 0){
+          System.out.println("Please provide some input.");
+        }
+        else{
+          System.out.println("One or more of your inputs exceeds the limitations of the integer object; "+ ex.getMessage());
+        }
+        errorState = 1;
+      }
+      finally{
+        if(numArgs != 2){ //If too many (or few) inputs, this runs and says so.
+          System.out.println("You have entered " + numArgs + " arguments. This program takes 2 arguments, and excess input will be ignored.");
+        }
+        if(errorState == 0){ //Final print of sum and values
+          a = Integer.parseInt(args[0]);
+          b = Integer.parseInt(args[1]);
+          int sum = (a + b);
+          System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        }
+      }
     }
 }
+
